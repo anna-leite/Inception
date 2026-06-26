bd601d62a197b4255a8b21e5a4b6d2d487812420
anna-leite <anna.leite@laplateforme.io>
2026-06-26 17:23:48 +0200

    change the Dockerfile for mariadb

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -3,20 +3,20 @@
 # stop the sript if a command fail
 set -e
 
-service mariadb start
-
 until mysqladmin ping -u root --silent; do
     sleep 1
 done
 
 mysql -u root <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIA_ROOT_PASSWORD}';
+
 CREATE DATABASE IF NOT EXISTS wordpress;
+
 CREATE USER IF NOT EXISTS '${MARIA_USER}'@'%' IDENTIFIED BY '${MARIA_PASSWORD}';
+
 GRANT ALL PRIVILEGES ON wordpress.* TO '${MARIA_USER}'@'%';
+
 FLUSH PRIVILEGES;
 EOF
-
-service mariadb stop
 
 exec mysqld_safe
